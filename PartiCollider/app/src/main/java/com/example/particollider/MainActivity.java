b96d7d3b293d2e78eab6722c0b6e1ea01405338c
rajahjac <jacob.rajah@mail.utoronto.ca>
2019-11-03 10:10:38 -0500

    Changes to movement

--- PartiCollider/app/src/main/java/com/example/particollider/MainActivity.java
+++ PartiCollider/app/src/main/java/com/example/particollider/MainActivity.java
@@ -23,8 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView particle1;
     private ImageView particle2;
     private ImageView particle3;
-    private ImageView particle4;
-    private ImageView particle5;
+
 
     public int width;
     public int height;
@@ -32,8 +31,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean started = true;
 
     Particle part1 = new Particle(1,1,1,1,1,1,1,1,true);
-    Particle part2 = new Particle(2,2,2, 2, -2, 2, 2,2, true);
-    Particle part3 = new Particle(3,1,3, 2, -2, 2, 3,4, true);
+    Particle part2 = new Particle(2,2,2, 2, 5, 2, 2,2, true);
+    Particle part3 = new Particle(3,1,3, 2, 10, 5, 3,4, true);
     ArrayList<Particle> particles = new ArrayList<>();
 
     Calculator calc = new Calculator();
@@ -44,10 +43,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         particle1 = findViewById(R.id.particle);
+        particle1.setY(part1.getyPos());
+        particle1.setX(part1.getxPos());
+
         particle2 = findViewById(R.id.particle2);
+        particle2.setY(part2.getyPos());
+        particle2.setX(part2.getxPos());
+
         particle3 = findViewById(R.id.particle3);
-        particle4 = findViewById(R.id.particle4);
-        particle5 = findViewById(R.id.particle5);
+        particle3.setY(part3.getyPos());
+        particle3.setX(part3.getxPos());
 
         // Finds the width and height of the screen
         Display display = getWindowManager().getDefaultDisplay();
@@ -81,8 +86,6 @@ public class MainActivity extends AppCompatActivity {
         ObjectAnimator anim = null;
         ObjectAnimator anim1 = null;
         ObjectAnimator anim2 = null;
-        ObjectAnimator anim3 = null;
-        ObjectAnimator anim4 = null;
 
         for (Particle p : particles) {
 
@@ -102,6 +105,7 @@ public class MainActivity extends AppCompatActivity {
                         "translationX", "translationY", path1);
                 long temp = (long) (1000 / p.getVelocity());
                 anim1.setDuration(temp);
+                System.out.println(p.getxPos());
                 //anim1.start();
             } else if (p.getPart() == 3) {
                 anim2 = ObjectAnimator.ofFloat(particle3,
@@ -109,18 +113,6 @@ public class MainActivity extends AppCompatActivity {
                 long temp = (long) (1000 / p.getVelocity());
                 anim2.setDuration(temp);
                 //anim2.start();
-            } else if (p.getPart() == 4) {
-                anim3 = ObjectAnimator.ofFloat(particle4,
-                        "translationX", "translationY", path1);
-                long temp = (long) (1000 / p.getVelocity());
-                anim3.setDuration(temp);
-                //anim3.start();
-            } else if (p.getPart() == 5) {
-                anim4 = ObjectAnimator.ofFloat(particle4,
-                        "translationX", "translationY", path1);
-                long temp = (long) (1000 / p.getVelocity());
-                anim4.setDuration(temp);
-                //anim.start();
             }
 
             p.setxPos(p.getxPos() + p.getxDir());
@@ -143,12 +135,6 @@ public class MainActivity extends AppCompatActivity {
             }
             if(i ==3){
                 particle3.setVisibility(View.VISIBLE);
-            }
-            if(i ==4){
-                particle4.setVisibility(View.VISIBLE);
-            }
-            if(i ==5){
-                particle5.setVisibility(View.VISIBLE);
             }
         }
     }
