bc82c1235383561c2cb545be830fd742e8f41b8f
blobbynord <45715642+blobbynord@users.noreply.github.com>
2019-11-02 23:36:21 -0400

    Calculator for final velocities and bouncing off walls

--- PartiCollider/app/src/main/java/com/example/particollider/Calculator.java
+++ PartiCollider/app/src/main/java/com/example/particollider/Calculator.java
@@ -40,8 +40,8 @@ public class Calculator {
 
             p1vxf=p1vx-((2*p2m)/(p1m+p2m))*(((p1vx-p2vx)*(p1px-p2px))/(e))*(p1px-p2px); //particle 1 final x velocity
             p1vyf=p1vy-((2*p2m)/(p1m+p2m))*(((p1vy-p2vy)*(p1py-p2py))/(e))*(p1py-p2py); //particle 1 final y velocity
-            p1vxf=p2vx-((2*p1m)/(p1m+p2m))*(((p2vx-p1vx)*(p2px-p1px))/(f))*(p2px-p1px); //particle 2 final x velocity
-            p1vyf=p2vy-((2*p1m)/(p1m+p2m))*(((p2vy-p1vy)*(p2py-p1py))/(f))*(p2py-p1py); //particle 2 final y velocity
+            p2vxf=p2vx-((2*p1m)/(p1m+p2m))*(((p2vx-p1vx)*(p2px-p1px))/(f))*(p2px-p1px); //particle 2 final x velocity
+            p2vyf=p2vy-((2*p1m)/(p1m+p2m))*(((p2vy-p1vy)*(p2py-p1py))/(f))*(p2py-p1py); //particle 2 final y velocity
         }
     }
 
